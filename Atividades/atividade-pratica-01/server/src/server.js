00c88bf8d0c1e03e99a968facbd7771a00c83409
Israel Amaral <64231004+imdoamaral@users.noreply.github.com>
2022-10-23 18:24:03 -0300

    Adiciona Get All e Create local de coleta

--- Atividades/atividade-pratica-01/server/src/server.js
+++ Atividades/atividade-pratica-01/server/src/server.js
@@ -4,6 +4,8 @@ import { estadoRouter } from './routes/estados.js';
 import { cidadeRouter } from './routes/cidades.js';
 import { sangueRouter } from './routes/tipos_sanguineos.js';
 import { pessoaRouter } from './routes/pessoas.js';
+import { localRouter } from './routes/locais_coleta.js';
+import { doacaoRouter } from './routes/doacoes.js';
 import cors from 'cors';
 
 const PORT = 4000;
@@ -17,6 +19,8 @@ app.use(estadoRouter);
 app.use(cidadeRouter);
 app.use(sangueRouter);
 app.use(pessoaRouter);
+app.use(localRouter);
+app.use(doacaoRouter);
 
 app.listen(PORT, () => {
     console.log(`[SERVER] Server is running on port ${PORT}.`);
