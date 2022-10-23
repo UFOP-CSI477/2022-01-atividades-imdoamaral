07ea102b8275cbb91218498e07cfd8d3701ddcf3
Israel Amaral <64231004+imdoamaral@users.noreply.github.com>
2022-10-23 18:24:18 -0300

    Adiciona Get All e Create Doações

--- Atividades/atividade-pratica-01/server/src/controllers/doacoes/GetAllDoacoesController.js
+++ Atividades/atividade-pratica-01/server/src/controllers/doacoes/GetAllDoacoesController.js
@@ -4,11 +4,13 @@ export class GetAllDoacoesController {
 
     async handle(request, response) {
 
-        const doacoes = await prismaClient.doacoes.findMany({
+        const doacoes = await prismaClient.doacao.findMany({
             include: {
                 local: true,
                 pessoa: true
             }
-        })
+        });
+
+        return response.json(doacoes);
     }
 }
